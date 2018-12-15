49f2fdbe55753987bccfcdf03d9988f31192d771
Clouds Flowing <clouds.flowing@gmail.com>
2018-12-15 16:59:09 +0800

    [analyze] make jupyter client

---
+++ analyze/jupyter/client.py
@@ -0,0 +1,123 @@
+from jupyter_client import KernelManager
+from jupyter_client.threaded import ThreadedKernelClient as KernelClient, ThreadedZMQSocketChannel as ZMQSocketChannel
+from jupyter_client.kernelspec import KernelSpec
+from jupyter_client.channels import HBChannel
+
+import os, json, datetime
+
+def find_ijulia(julia_path='/usr/bin/julia'):
+    from subprocess import check_output
+    return check_output([julia_path, '-e', 'import IJulia; print(pathof(IJulia))']).decode()
+
+def julia_spec(kernel_path=None, *, julia_path='/usr/bin/julia'):
+    if kernel_path is None:
+        kernel_path = os.path.join(os.path.dirname(find_ijulia()), "kernel.jl")
+    julia_argv=[julia_path,'-i','--startup-file=yes','--color=yes',kernel_path,'{connection_file}']
+    return KernelSpec(display_name="julia", env={}, language="julia", argv=julia_argv)
+
+def json_default(obj):
+    if isinstance(obj, datetime.datetime):
+        return str(obj)
+    else:
+        debug_print("dump", str(type(obj)))
+    return repr(obj)
+
+def channel_print(channel, msg):
+    try:
+        msg = json.dumps(msg, default=json_default)
+    except:
+        pass
+    print(f"{channel}>{msg}")
+
+def debug_print(*args):
+    msg = args
+    if len(args) == 1:
+        msg = args[0]
+    channel_print("debug", msg)
+
+class StdoutHandler:
+    channel_name = "dummy"
+    def call_handlers(self, msg):
+        channel_print(self.channel_name, msg)
+
+class HBOutputChannel(StdoutHandler, HBChannel):
+    channel_name = "hb"
+class ShellOutputChannel(StdoutHandler, ZMQSocketChannel):
+    channel_name = "shell"
+class StdInOutputChannel(StdoutHandler, ZMQSocketChannel):
+    channel_name = "stdin"
+class IOPubOutputChannel(StdoutHandler, ZMQSocketChannel):
+    channel_name = "iopub"
+
+supported_request = [
+    'comm_info_request',
+    'complete_request',
+    'execute',
+    'execute_request',
+    'history_request',
+    'input_reply',
+    'input_request',
+    'inspect_request',
+    'is_complete_request',
+    'kernel_info_request',
+    'msg',
+    'shutdown_request',
+]
+
+def gen_msg(kc, msg):
+    msg = json.loads(msg)
+    if isinstance(msg, dict):
+        msg_type = msg["msg_type"]
+        content = msg["content"]
+    elif isinstance(msg, list):
+        msg_type, content = msg
+    else:
+        debug_print("input", str(type(msg)))
+    if msg_type not in supported_request:
+        debug_print("input", msg_type)
+    return kc.session.msg(msg_type, content)
+
+def start_kernel(spec):
+    km = KernelManager(kernel_name=spec.language)
+    km._kernel_spec = spec
+    km.start_kernel()
+    return km
+
+def connect_kernel(connection_file):
+    kc = KernelClient(
+        hb_channel_class=HBOutputChannel,
+        shell_channel_class=ShellOutputChannel,
+        stdin_channel_class=StdInOutputChannel,
+        iopub_channel_class=IOPubOutputChannel)
+    kc.load_connection_file(connection_file)
+    kc.start_channels()
+    return kc
+
+def event_loop(kc):
+    channels = {
+        "hb": kc.hb_channel,
+        "shell": kc.shell_channel,
+        "stdin": kc.stdin_channel,
+        "iopub": kc.iopub_channel,
+    }
+    while True:
+        try:
+            line = input()
+            [channel, msg] = line.split(">", 1)
+            msg = gen_msg(kc, msg)
+            channels[channel].send(msg)
+            debug_print("msgid", msg["header"]["msg_id"])
+        except KeyboardInterrupt:
+            debug_print("exit", "interrupt")
+            break
+        except Exception as e:
+            debug_print("error", e)
+
+
+if __name__ == "__main__":
+    import readline
+    spec = julia_spec()
+    km = start_kernel(spec)
+    kc = connect_kernel(km.connection_file)
+    event_loop(kc)
+    kc.ioloop_thread.join()
